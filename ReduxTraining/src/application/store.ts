2f526a04d9ddd8d2fb646726054af480a479c686
0xUndersun <alex.atc.dev@gmail.com>
2023-07-24 19:04:32 +0200

    [Update] Add infrastructure for manage localstorage, update readme, store

--- ReduxTraining/src/application/store.ts
+++ ReduxTraining/src/application/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../domain/todoSlice";
+import { saveState, loadState } from "../infrastructure/localStorage";
+
+const persistedState = loadState();
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  preloadedState: persistedState,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+store.subscribe(() => {
+  saveState(store.getState());
+});
 export type AppDispatch = typeof store.dispatch;
